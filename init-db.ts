1e530929635cf38610fd2efc3c3924ca027d45f2
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-07-30 12:37:44 -0400

    Simplify data model & make generic

--- init-db.ts
+++ init-db.ts
@@ -4,32 +4,32 @@ import { MeiliSearch } from "meilisearch";
 import { $, cd, within, ProcessOutput, ProcessPromise } from "zx";
 import { mkdirp } from "mkdirp";
 
-const bin = `.bin`;
-const meilibin = `${bin}/meilisearch`;
-const meiliUrl = "http://127.0.0.1:7700";
+const BIN = `.bin`;
+const MEILI_BIN = `${BIN}/meilisearch`;
+const MEILI_URL = "http://127.0.0.1:7700";
 
 zxMain(async () => {
   await downloadMeili();
 
-  const meiliProcess = $`${meilibin} --db-path .out/search-index.ms --no-analytics`;
+  const meiliProcess = $`${MEILI_BIN} --db-path .out/search-index.ms --no-analytics`;
 
   const client = new MeiliSearch({
-    host: meiliUrl,
+    host: MEILI_URL,
   });
   await waitFor(() => client.isHealthy());
 
-  await setUpIndex(client);
-  await uploadIndex(`index-headings.ndjson`, "headings");
+  await setUpIndex(client, "movies");
+  await uploadIndex(`movies.ndjson`, "movies");
 
   await waitFor(() => client.isHealthy());
   await killProcess(meiliProcess);
 });
 
 async function downloadMeili(): Promise<void> {
-  if (fs.existsSync(meilibin)) return;
+  if (fs.existsSync(MEILI_BIN)) return;
   await within(async () => {
-    await mkdirp(bin);
-    cd(bin);
+    await mkdirp(BIN);
+    cd(BIN);
 
     // Keep version in sync with Dockerfile
     process.env.MEILI_VERSION = "v1.9.0";
@@ -37,18 +37,15 @@ async function downloadMeili(): Promise<void> {
   });
 }
 
-async function setUpIndex(client: MeiliSearch): Promise<void> {
-  await client.createIndex("headings", { primaryKey: "id" });
-
-  const filterableAttributes = ["module", "package", "package-version"];
-  await client.index("headings").updateSettings({
+async function setUpIndex(client: MeiliSearch, index: string): Promise<void> {
+  await client.createIndex(index, { primaryKey: "id" });
+  await client.index(index).updateSettings({
     searchableAttributes: ["title"],
-    filterableAttributes,
   });
 }
 
 async function uploadIndex(file: string, index: string): Promise<void> {
-  await $`cat ${file} | gzip | curl -s -X POST '${meiliUrl}/indexes/${index}/documents' --data-binary @- -H 'Content-Type: application/x-ndjson' -H 'Content-Encoding: gzip'`;
+  await $`cat ${file} | gzip | curl -s -X POST '${MEILI_URL}/indexes/${index}/documents' --data-binary @- -H 'Content-Type: application/x-ndjson' -H 'Content-Encoding: gzip'`;
 }
 
 // ------------------------------------------------------------------------
@@ -78,35 +75,16 @@ async function killProcess(process: ProcessPromise) {
   }
 }
 
-type WaitFoOptions = {
-  wait?: number;
-  timeout?: number;
-};
-
 function wait(ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-async function waitFor(
-  fn: () => boolean | Promise<boolean>,
-  options: WaitFoOptions = {},
-) {
-  options = {
-    wait: 10,
-    ...options,
-  };
+async function waitFor(fn: () => boolean | Promise<boolean>) {
   let result = false;
-  let timedOut = false;
-  const startTime = new Date().getTime();
-
-  while (!result && !timedOut) {
-    const currentTime = new Date().getTime();
+  while (!result) {
     result = await fn();
-    if (!result) await wait(options.wait!);
-    if (options.timeout) {
-      timedOut = currentTime - startTime > options.timeout;
-    }
+    if (!result) await wait(10);
   }
 }
--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 rm -rf .out
 
-node -r esbuild-register create-index.ts
+node -r esbuild-register init-db.ts
 docker build -t meilisearch-debug .
 docker run -p 7700:7700 --init --rm meilisearch-debug
